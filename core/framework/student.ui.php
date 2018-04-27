e5cbd5e69999e0ed517f2f7aebc560205c294b12
Phoenix Eve Aspacio <aspaciop@gmail.com>
2018-04-27 12:28:02 +0800

    Add files via upload

--- core/framework/student.ui.php
+++ core/framework/student.ui.php
@@ -1,110 +1,234 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <style>
-		html {
-		  font-size: 14px;
-		}
-		@media (min-width: 768px) {
-		  html {
-			font-size: 16px;
-		  }
-		}
-
-		.container {
-		  max-width: 960px;
-		}
-
-		.pricing-header {
-		  max-width: 700px;
-		}
-
-		.card-deck .card {
-		  min-width: 220px;
-		}
-
-		.border-top { border-top: 1px solid #e5e5e5; }
-		.border-bottom { border-bottom: 1px solid #e5e5e5; }
-
-		.box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); }
-		
-		.active {
-			background-color: #f1f1f1;
-			border-radius: 3px;
-		}
-	</style>
-  </head>
+<?php include_once('./core/partials/html.panel.head.php'); ?>
 
   <body>
 
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-      <h5 class="my-0 mr-md-auto font-weight-normal">Guidance Services</h5>
-      <nav class="my-2 my-md-0 mr-md-3">
-        <a class="p-2 text-dark<?=basename($_SERVER["SCRIPT_FILENAME"])=='panel.php'?' active':'';?>" href="./panel.php">Profile</a>
-        <a class="p-2 text-dark" href="#">Announcements</a>
-        <a class="p-2 text-dark" href="#">Support</a>
-        <a class="p-2 text-dark" href="#">Schedule</a>
-      </nav>
+    <div class="perm-one d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <h5 class="my-0 mr-md-auto font-weight-normal" style="font-family: Anton;">Guidance Services</h5>
+	  <div>
+	  <nav class="my-2 my-md-0 mr-md-3" style="position: relative; display: flex; overflow-x: auto; overflow-y: hidden; max-width: 100%;">
+        <a class="nav-item-spacing p-2 text-dark<?=basename($_SERVER["SCRIPT_FILENAME"])=='panel.php'?' active':'';?>" href="/index.php">Panel</a>
+        <a class="nav-item-spacing p-2 text-dark" href="#">Edit</a>
+        <a class="nav-item-spacing p-2 text-dark" href="#">Settings</a>
+      </nav></div>
       <a class="btn btn-outline-primary" href="./signout.php">Sign out</a>
     </div>
 
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1><?=trim($user_data->first_name.' <i>'.$user_data->middle_name.'</i> '.$user_data->last_name.' '.$user_data->name_suffix);?></h1>
-      <p class="lead"><?=strtoupper($_SESSION['type'].' '.$user_data->student_level.' '.$user_data->student_section);?></p>
+    <div class="swamp-main px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1><?=trim($user_data->first_name.(!empty($user_data->middle_name)?' <i>'.$user_data->middle_name.'</i> ':' ').$user_data->last_name.' '.$user_data->name_suffix);?></h1>
+      <span class="badge badge-primary">STUDENT</span>
+	  <p class="lead"><?=$_SESSION['id_no'];?></p>
+	  <!--<p class="lead"><?=trim(strtoupper($user_data->student_level.' '.$user_data->student_section));?></p>-->
     </div>
 
 	<div class="container">
       <div class="card-deck mb-3 text-center">
         <div class="card mb-4 box-shadow">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Free</h4>
+          <span onclick="kebabProp('kebabPropID')" style="cursor: pointer;" data-toggle="collapse" data-target="#PerInfCOLLAPSE" aria-expanded="false">
+		  <div class="card-header">
+            <h4 id="personal-information" class="my-0 font-weight-normal" title="your confidential and important info">Personal Information</h4>
           </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+		  <img title="expand" id="kebabPropID" src="/core/assets/octicons-7.2.0/build/svg/kebab-horizontal.svg">
+		  <!--<img src="/core/assets/octicons-7.2.0/build/svg/chevron-down.svg">-->
+		  </span>
+          <div class="card-body collapse" id="PerInfCOLLAPSE">
+
+  
+   <div class="card-deck">
+   <div class="card mb-4 box-shadow">
+ <table style="text-align: left; margin-bottom: 0px;" class="table table-striped table-sm">
+   <tr>
+    <td>Address</td>
+    <td><b><?=trim($user_data->address);?></b></td>
+  </tr>
+  <tr>
+    <td>Birthdate</td>
+    <td><b><?=trim($user_data->birth_month.'/'.$user_data->birth_day.'/'.$user_data->birth_year);?></b></td>
+  </tr>
+  <tr>
+    <td>Email</td>
+    <td><b><?=trim($user_data->email);?></b></td>
+  </tr>
+  <tr>
+    <td>Contact Number</td>
+    <td><b><?=trim($user_data->contact_number);?></b></td>
+  </tr>
+  <tr>
+    <td>Religion</td>
+    <td><b><?=trim($user_data->religion);?></b></td>
+  </tr>
+  <tr>
+    <td>Nationality</td>
+    <td><b><?=trim($user_data->nationality);?></b></td>
+  </tr>
+  <tr>
+    <td>Height</td>
+    <td><b><?=trim($user_data->height);?></b></td>
+  </tr>
+  <tr>
+    <td>Weight</td>
+    <td><b><?=trim($user_data->weight);?></b></td>
+  </tr>
+  </table>
+  </div>
+  
+  
+   <div class="card mb-4 box-shadow">
+ <table style="text-align: left; margin-bottom: 0px;" class="table table-striped table-sm">
+  <tr>
+    <td>Level</td>
+    <td><b><?=trim($user_data->student_level);?></b></td>
+  </tr>
+  <tr>
+    <td>Section</td>
+    <td><b><?=trim($user_data->student_section);?></b></td>
+  </tr>
+  <tr>
+    <td>Adviser</td>
+    <td><b><?=trim($user_data->student_adviser);?></b></td>
+  </tr>
+  <tr>
+    <td>Life Motto</td>
+    <td><b><?=trim($user_data->life_motto);?></b></td>
+  </tr>
+  <tr>
+    <td>Life Ambition</td>
+    <td><b><?=trim($user_data->life_ambition);?></b></td>
+  </tr>
+  <tr>
+    <td>Hobbies</td>
+    <td><b><?=trim($user_data->hobbies);?></b></td>
+  </tr>
+  </table>
+ </div>
+ </div>
+ 
+ 
+ <div class="card-deck">
+ <div class="card mb-4 box-shadow">
+ 
+ <table style="text-align: left; margin-bottom: 0px;" class="table table-striped table-sm">
+   <tr>
+  <td>Mother's Name</td>
+    <td><b><?=trim($user_data->mother_name);?></b></td>
+  </tr>
+   <tr>
+  <td>Mother's Occupation</td>
+    <td><b><?=trim($user_data->mother_occupation);?></b></td>
+  </tr>
+  <td>Mother's Contact Number</td>
+    <td><b><?=trim($user_data->mother_contact_number);?></b></td>
+  </tr>
+ </table>
+ </div>
+  <div class="card mb-4 box-shadow">
+ <table style="text-align: left; margin-bottom: 0px;" class="table table-striped table-sm">
+  <td>Father's Name</td>
+    <td><b><?=trim($user_data->father_name);?></b></td>
+  </tr>
+   <tr>
+  <td>Father's Occupation</td>
+    <td><b><?=trim($user_data->father_occupation);?></b></td>
+  </tr>
+  <td>Father's Contact Number</td>
+    <td><b><?=trim($user_data->father_contact_number);?></b></td>
+  </tr>
+</table>
+
+
+</div>
+
+</div>
+<i>Note: Please contact the counselor/admin to make changes to your personal profile.</i>
           </div>
         </div>
+		
+
+		
+    
+      </div>
+
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  <div class="card-deck mb-3 text-center">
         <div class="card mb-4 box-shadow">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Pro</h4>
+          <span onclick="kebabProp('kebabPropID2')" style="cursor: pointer;" data-toggle="collapse" data-target="#AnnCOLLAPSE"  aria-expanded="true">
+		  <div class="card-header">
+            <h4 class="my-0 font-weight-normal" title="important broadcasts from the admin">Announcements</h4>
           </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>20 users included</li>
-              <li>10 GB of storage</li>
-              <li>Priority email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
-          </div>
-        </div>
-        <div class="card mb-4 box-shadow">
-          <div class="card-header">
-            <h4 class="my-0 font-weight-normal">Enterprise</h4>
-          </div>
-          <div class="card-body">
-            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>30 users included</li>
-              <li>15 GB of storage</li>
-              <li>Phone and email support</li>
-              <li>Help center access</li>
-            </ul>
-            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+		  <img title="expand" id="kebabPropID2" style="display: none;" src="/core/assets/octicons-7.2.0/build/svg/kebab-horizontal.svg">
+		  </span>
+          <div class="card-body collapse show" id="AnnCOLLAPSE">
+
+
+
+<?php
+$scan = scandir('./data/public_announcements/');
+$f_count = count($scan)-2;
+if($f_count > 0) {
+	$i = 0;
+	foreach ($scan as $pa_file) {
+		$file = './data/public_announcements/' . $pa_file;
+		if(!is_dir($file)) {
+			echo '<h4>'.json_decode(file_get_contents($file))->title.'</h4>';
+			echo json_decode(file_get_contents($file))->body;
+			if(++$i < $f_count) {
+				echo '<hr>';
+			}
+		} elseif($pa_file != '.' && $pa_file != '..') {
+			//rmdir($file);
+			$f_count = $f_count-1;
+		}
+	}
+} else {
+	echo '<span class="badge badge-secondary">NONE</span>';
+}
+?>
+
+
+
+
+
           </div>
         </div>
       </div>
-
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
       <footer class="pt-4 my-md-5 pt-md-5 border-top">
 	  <p class="text-muted">&copy; St. Paul University Dumaguete <?=date('Y');?></p>
         <!--<div class="row">
@@ -148,8 +272,18 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script>
+		function kebabProp(idDef) {
+			if(document.getElementById(idDef).parentNode.getAttribute("aria-expanded") == 'false') {
+				document.getElementById(idDef).style.cssText = 'display: none;';
+			}
+			if(document.getElementById(idDef).parentNode.getAttribute("aria-expanded") == 'true') {
+				document.getElementById(idDef).style.cssText = 'display: initial;';
+			}
+		}
+	</script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="./core/assets/bootstrap.min.js"></script>
   </body>
 </html>
---
+++ core/partials/html.panel.head.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./core/assets/bootstrap.min.css">
+    <style>
+		html {
+		  font-size: 14px;
+		}
+		@media (min-width: 768px) {
+		  html {
+			font-size: 16px;
+		  }
+		}
+		
+		@media (max-width: 768px) {
+		  .swamp-main {
+		    max-width: 700px;
+	  	    margin-top: 150px!important;
+		  }
+		}
+
+		.container {
+		  max-width: 960px;
+		}
+
+		.swamp-main {
+		  max-width: 700px;
+		  margin-top: 80px;
+		}
+
+		.card-deck .card {
+		  min-width: 220px;
+		  height: max-content;
+		}
+
+		.border-top { border-top: 1px solid #e5e5e5; }
+		.border-bottom { border-bottom: 1px solid #e5e5e5; }
+
+		.box-shadow { box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05); }
+		
+		.active {
+			background-color: #f1f1f1;
+			border-radius: 3px;
+		}
+		
+		.nav-item-spacing {
+			margin-left: 5px;
+		}
+		
+		.nav-item-spacing::-webkit-scrollbar { 
+			display: none;
+		}
+		.perm-one {
+			position: fixed;
+			width: 100%;
+			z-index: 1;
+			top: 0px;
+		}
+		table > tbody > tr > td {
+			white-space: pre;
+			white-space: pre-wrap;
+			white-space: pre-line;
+			white-space: -pre-wrap;
+			white-space: -o-pre-wrap;
+			white-space: -moz-pre-wrap;
+			white-space: -hp-pre-wrap;
+			word-wrap: break-word;
+		}
+	</style>
+	<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet'>
+  </head>
